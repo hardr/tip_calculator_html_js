edac2aecb88061e01e96d798cbeca4b579c60712
Ryan Harings <ryanharings@hotmail.com>
2016-07-20 18:02:08 -0600

    linted and validated

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,28 @@
 $(document).ready(function() {
-  console.log("ready!");
+  console.log('ready!');
 
 });
 
 $('.form1').submit(function(event) {
   event.preventDefault();
-  var tip = ($('.input1').val() * .2).toFixed(2);
+  var tip = ($('.input1').val() * 0.20).toFixed(2);
   $('.results1').append('<h2>You should tip $' + tip + '</h2>');
-})
+});
 
 $('.form2').submit(function(event) {
   event.preventDefault();
   var bill = $('.input2').val();
 
-  var tip = (bill.replace('$', '') * .20).toFixed(2);
+  var tip = (bill.replace('$', '') * 0.20).toFixed(2);
 
   $('.results2').append('<h2>You should tip $' + tip + '</h2>');
-})
+});
+
+$('.form3').submit(function(event) {
+  event.preventDefault();
+  var bill = $('.input3').val();
+  var tipAmount = $('.tipAmount').val();
+  var tip = (bill.replace('$', '') * tipAmount).toFixed(2);
+
+  $('.results3').append('<h2>You should tip $' + tip + '</h2>');
+});
